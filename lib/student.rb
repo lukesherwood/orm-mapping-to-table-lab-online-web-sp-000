4c2dd7d96f3dc518a9c769b18fccd008bde8355f
Luke Sherwood <luke.sherwood55@gmail.com>
2020-03-04 23:30:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,14 @@ def intialize(id=nil, name, grade)
 end
 
 def self.create_table
-  
+   sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
 end
 
 end
