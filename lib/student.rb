040537c71374276042fe64ed352f650a69e0ee15
Luke Sherwood <luke.sherwood55@gmail.com>
2020-03-04 23:34:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,7 @@ end
 
 def self.drop_table
   sql =  <<-SQL 
-      DROP TABLE IF NOT EXISTS students (
+      DROP TABLE IF EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
         grade TEXT
