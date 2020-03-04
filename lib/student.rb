54c5482983e47456c549b5cf7416b419ef86064e
Luke Sherwood <luke.sherwood55@gmail.com>
2020-03-04 23:29:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
 class Student
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn] 
+attr_accessor :name, :grade
+attr_reader :id
+def intialize(id=nil, name, grade)
+  @id = id
+  @name = name
+  @grade = grade
+end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+def self.create_table
   
+end
+
 end
